a521e9a90e9e61294e4f76015ade2157bf5e5b53
ROKI MONDAL <rokimondal@ROKIs-MacBook-Air.local>
2026-01-22 16:22:31 +0530

    update branch

---
+++ Dynamic Programming/MountainRanges.java
@@ -0,0 +1,23 @@
+public class MountainRanges {
+    public static int countMountainRanges(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+
+        int dp[] = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countMountainRanges(5));
+    }
+}
