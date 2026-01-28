f87b65f627d5159f02778f799e210fce3e7657f5
ROKI MONDAL <rokimondal@ROKIs-MacBook-Air.local>
2026-01-29 01:39:51 +0530

    solve mcm

--- Dynamic Programming/MCM.java
+++ Dynamic Programming/MCM.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MCM {
 
     public static int mcm(int arr[], int i, int j) {
@@ -17,8 +19,66 @@ public class MCM {
         return ans;
     }
 
+    public static int mcmMemoization(int arr[], int i, int j, int dp[][]) {
+        if (i == j)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = mcmMemoization(arr, i, k, dp);
+            int cost2 = mcmMemoization(arr, k + 1, j, dp);
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+        }
+
+        return dp[i][j] = ans;
+    }
+
+    public static int mcmTabulation(int arr[]) {
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 0;
+        }
+
+        for (int len = 2; len <= n - 1; len++) {
+            for (int i = 1; i <= n - len; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j - 1; k++) {
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k + 1][j];
+                    int cost3 = arr[i - 1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1 + cost2 + cost3);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return dp[1][n - 1];
+    }
+
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 3 };
-        System.out.println(mcm(arr, 1, arr.length - 1));
+
+        int dp[][] = new int[arr.length][arr.length];
+
+        for (int row[] : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        System.out.println(mcmTabulation(arr));
     }
 }
